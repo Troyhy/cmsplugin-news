2984e3c49433a734156ba1d846295ec02e6d6ae7
Harro van der Klauw <hvdklauw@gmail.com>
2009-10-29 10:16:57 +0100

    Added setup.py cmsplugin-news is now available from pypi.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cmsplugin-news',
+    version='0.1a4',
+    description='This is a news app/plugin for the django-cms 2',
+    author='Harro van der Klauw',
+    author_email='hvdklauw@gmail.com',
+    url='http://bitbucket.org/MrOxiMoron/cmsplugin-news/',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools', 'setuptools_bzr'],
+)
