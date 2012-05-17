8c8a020d6cd2e7756ef02d80d6f8eaf55cba0b70
Horst Gutmann <zerok@zerokspot.com>
2012-05-17 09:06:13 +0200

    Move to class-based generic views and queryset fix on views
    Previously the queryset was only fetched when the urlpatterns
    were generated. This led to news items not being accessible if
    they were created with a date of after this reloading took place.
    
    Now the queryset for all date-based views is generated on the
    fly in order to operate on the current timestamp.

--- cmsplugin_news/urls.py
+++ cmsplugin_news/urls.py
@@ -1,37 +1,24 @@
 from django.conf.urls.defaults import *
 
-from cmsplugin_news.models import News
-
 from . import feeds
 from . import views
 
 
-news_info_dict = {
-    'queryset': News.published.all(),
-    'date_field': 'pub_date',
-}
-
-news_info_month_dict = {
-    'queryset': News.published.all(),
-    'date_field': 'pub_date',
-    'month_format': '%m',
-}
-
 urlpatterns = patterns('django.views.generic.date_based',
     url(r'^$',
-        views.ArchiveIndexView.as_view(), news_info_dict, name='news_archive_index'),
+        views.ArchiveIndexView.as_view(), name='news_archive_index'),
 
     url(r'^(?P<year>\d{4})/$',
-        'archive_year', news_info_dict, name='news_archive_year'),
+        views.YearArchiveView.as_view(), name='news_archive_year'),
 
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$',
-        'archive_month', news_info_month_dict, name='news_archive_month'),
+        views.MonthArchiveView.as_view(), name='news_archive_month'),
 
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$',
-        'archive_day', news_info_month_dict, name='news_archive_day'),
+        views.DayArchiveView.as_view(), name='news_archive_day'),
 
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
-        'object_detail', news_info_month_dict, name='news_detail'),
+        views.DetailView.as_view(), name='news_detail'),
 
     url(r'^feed/$', feeds.NewsFeed())
 )
--- cmsplugin_news/views.py
+++ cmsplugin_news/views.py
@@ -1,10 +1,19 @@
-from django.views.generic import ListView
+from django.views import generic as generic_views
 
 from . import models
 from . import settings
 
 
-class ArchiveIndexView(ListView):
+class PublishedNewsMixin(object):
+    """
+    Since the queryset also has to filter elements by timestamp
+    we have to fetch it dynamically.
+    """
+    def get_queryset(self):
+        return models.News.published.all()
+
+
+class ArchiveIndexView(PublishedNewsMixin, generic_views.ListView):
     """
     A simple archive view that exposes following context:
 
@@ -20,9 +29,9 @@ class ArchiveIndexView(ListView):
     ListView.
     """
     paginate_by = settings.ARCHIVE_PAGE_SIZE
-    queryset = models.News.published.all()
     template_name = 'cmsplugin_news/news_archive.html'
     include_yearlist = True
+    date_field = 'pub_date'
 
     def get_context_data(self, **kwargs):
         context = super(ArchiveIndexView, self).get_context_data(**kwargs)
@@ -31,3 +40,23 @@ class ArchiveIndexView(ListView):
             date_list = self.get_queryset().dates('pub_date', 'year')[::-1]
             context['date_list'] = date_list
         return context
+
+
+class DetailView(PublishedNewsMixin, generic_views.DateDetailView):
+    month_format = '%m'
+    date_field = 'pub_date'
+
+
+class MonthArchiveView(PublishedNewsMixin, generic_views.MonthArchiveView):
+    month_format = '%m'
+    date_field = 'pub_date'
+
+
+class YearArchiveView(PublishedNewsMixin, generic_views.YearArchiveView):
+    month_format = '%m'
+    date_field = 'pub_date'
+
+
+class DayArchiveView(PublishedNewsMixin, generic_views.DayArchiveView):
+    month_format = '%m'
+    date_field = 'pub_date'
